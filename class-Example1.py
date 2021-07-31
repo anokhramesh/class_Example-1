1cbe41de7c058c7f13dd33d856cfa18599d7b7f0
anokhramesh <67797819+anokhramesh@users.noreply.github.com>
2021-07-31 19:54:41 +0400

    Add files via upload

---
+++ class-Example1.py
@@ -0,0 +1,12 @@
+class Person:# create a class with a class keyword and a class name end with a column
+  def __init__(self, name, age):
+    self.name = name
+    self.age = age
+
+p1 = Person("biju", 40)
+p2 = Person("ramesh",50)
+
+print(p1.name)
+print(p1.age)
+print(p2.name)
+print(p2.age)
